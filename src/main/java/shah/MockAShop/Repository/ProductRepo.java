f34cce20e201a00e971312ce84ab918b330f1616
Norulshahlam <norulshahlam@gmail.com>
2021-07-15 22:02:32 +0800

    d

--- src/main/java/shah/MockAShop/Repository/ProductRepo.java
+++ src/main/java/shah/MockAShop/Repository/ProductRepo.java
@@ -1,8 +1,5 @@
 package shah.MockAShop.Repository;
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import shah.MockAShop.Models.Product;
 
@@ -12,4 +9,3 @@ public interface ProductRepo extends JpaRepository<Product, Integer> {
   // @Query(value="select product_id, seller_id from product", nativeQuery=true)
   // List<Product> findAll();
 }
-
